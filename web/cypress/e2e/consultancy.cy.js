9b1e092395817fb4d799db12140b6933fe59b4b1
yuri-sl <yuri03ysl@gmail.com>
2025-07-20 10:07:01 -0300

    SEÇÃO 3

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -4,7 +4,10 @@ describe("Formulário de consultoria", () => {
     cy.submitLoginForm("papito@webdojo.com", "katana123");
 
     cy.goTo("Formulários", "Consultoria");
-    cy.get('input[placeholder="Digite seu nome completo"]').type("PapitoNinja");
+    cy.get('input[placeholder="Digite seu nome completo"]').type(
+      "Papito Ninja"
+    );
+    cy.get('input[placeholder="Digite seu email"]').type("papito@gmail.com");
     cy.get('input[placeholder="(00) 00000-0000"]')
       .type("11 99999-1000")
       .should("have.value", "(11) 99999-1000");
@@ -14,6 +17,71 @@ describe("Formulário de consultoria", () => {
       .parent()
       .find("select")
       .select("inCompany");
+    cy.contains("span", "Pessoa Física").parent().find("input").check();
+    ////span[text()="Pessoa Física"]//..//input
+    cy.contains("span", "Pessoa Jurídica").parent().find("input").check();
+    cy.contains("span", "Pessoa Física")
+      .parent()
+      .find("input")
+      .check()
+      .should("be.checked");
+
+    cy.contains("label", "Pessoa Jurídica")
+      .find("input")
+      .should("be.not.checked");
+    cy.contains("label", "CPF").parent().find("input");
+
+    cy.get('input[placeholder="000.000.000-00"]')
+      .type("22654312022")
+      .should("have.value", "226.543.120-22");
+
+    const discoveryChannels = [
+      "Instagram",
+      "Udemy",
+      "YouTube",
+      "Indicação de Amigo",
+    ];
+
+    discoveryChannels.forEach((channel) => {
+      cy.contains("span", channel)
+        .parent()
+        .find("input[type='checkbox']")
+        .click()
+        .should("be.checked");
+    });
+
+    cy.get('input[type="file"]').selectFile("./cypress/fixtures/1.pdf", {
+      force: true,
+    });
+    cy.get(
+      'textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]'
+    )
+      .type("Argentina Prateada")
+      .should("have.value", "Argentina Prateada");
+    const techs = [
+      "Cypress",
+      "Selenium",
+      "WebDriverIO",
+      "PlayWright",
+      "Robot Framework",
+    ];
+    techs.forEach((tech) => {
+      cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+        .type(tech)
+        .type("{enter}");
+      cy.contains("label", "Tecnologias")
+        .parent()
+        .contains("span", tech)
+        .should("be.visible");
+    });
+    cy.contains("label", "termos de uso")
+      .find("input")
+      .check()
+      .should("be.checked");
+    cy.contains("button", "Enviar formulário").click();
+    cy.contains(
+      "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido."
+    ).should("be.visible");
   });
 });
 
