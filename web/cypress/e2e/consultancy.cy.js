a3232300c2449ac49e9260ebd6d496d73b34d89e
yuri <yuri03ysl@gmail.com>
2025-07-26 07:32:19 -0300

    A lot of updates

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,5 +1,5 @@
 describe("Formulário de consultoria", () => {
-  it("Deve solicitar consultoria individual", () => {
+  it.only("Deve solicitar consultoria individual", () => {
     cy.start();
     cy.submitLoginForm("papito@webdojo.com", "katana123");
 
@@ -79,14 +79,45 @@ describe("Formulário de consultoria", () => {
       .check()
       .should("be.checked");
     cy.contains("button", "Enviar formulário").click();
-    cy.contains(
-      "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido."
-    ).should("be.visible");
+    cy.wait(2000);
+    cy.get('.modal', {timeout: 7000})
+      .should('be.visible')
+      .find('.modal-content')
+      .should('be.visible')
+      .and('have.text',"Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.")
 
-    //Verificar Classe e CSS de componente
-    cy.contains('p','Digite nome e sobrenome').should('be.visible').and('have.class','text-red-400').and('have.css','color','rgb(248, 113, 113)');//Asserção que garante a verificação da classe do CSS do elemento
-    cy.contains('p','Informe um email válido').should('be.visible');
   });
+  it("Deve conter uma solicitação vazia", () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com','katana123');
+    cy.goTo('Formulários','Consultoria');
+    cy.contains('button','Enviar formulário').click()
+    //Verificar Classe e CSS de componente
+    cy.contains('label','Nome Completo *')
+      .parent()
+      .find('p')
+      .should('be.visible')
+      .should('have.text','Campo obrigatório')
+      .and('have.class','text-red-400')
+      .and('have.css','color','rgb(248, 113, 113)');//Asserção que garante a verificação da classe do CSS do elemento
+
+    cy.contains('label','Email *')
+      .parent()
+      .find('p')
+      .should('be.visible')
+      .should('have.text','Campo obrigatório')
+      .and('have.class','text-red-400')
+      .and('have.css','color','rgb(248, 113, 113)');//Asserção que garante a verificação da classe do CSS do elemento
+    //cy.contains('p','Campo obrigatório').should('be.visible');
+    cy.contains('label','termos de uso')
+    .parent()
+    .find('p')
+    .should('be.visible')
+    .should('have.text','Você precisa aceitar os termos de uso')
+    .and('have.class','text-red-400')
+    .and('have.css','color','rgb(248, 113, 113)');//Asserção que garante a verificação da classe do CSS do elemento
+
+  })
 });
 
 Cypress.Commands.add("goTo", (buttonName, pageTitle) => {
