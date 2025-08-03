56968a26d53c4ad0dcbcea5b99b3da30cde2a8db
yuri-sl <yuri03ysl@gmail.com>
2025-08-03 10:31:31 -0300

    numeroVariavel

---
+++ web/cypress/e2e/number.cy.js
@@ -0,0 +1,26 @@
+let variavelNum = 0;
+
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/random_num.html");
+  });
+  it("gera um número ao clicar e pesquisa por ele", () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get("button").click();
+    cy.get("#numero")
+      .invoke("text")
+      .then((text) => {
+        const numeroGerado = text.match(/\d+/)[0];
+        cy.wrap(numeroGerado).as("numeroSalvo");
+      });
+    //reutilizando o alias
+    cy.get("@numeroSalvo").then((valor) => {
+      cy.get("#pesquisa").clear().type(valor);
+    });
+    //pode reutilizar aqui
+    cy.get("@numeroSalvo").then((valor) => {
+      cy.log("Número gerado e salvo: " + valor);
+    });
+    /* ==== End Cypress Studio ==== */
+  });
+});
